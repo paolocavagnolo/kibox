fea5de1f1bc74de48f8c74c2844d6ba946f91966
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2024-03-26 00:14:44 +0100

    Create redul_led.ino

---
+++ redbul_led/redul_led.ino
@@ -0,0 +1,125 @@
+#define FASTLED_ALLOW_INTERRUPTS 0
+#include <FastLED.h>
+FASTLED_USING_NAMESPACE
+
+#define NUM_LEDS 214
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  // put your setup code here, to run once:
+  delay(3000);
+  FastLED.addLeds<LED_TYPE, 2, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, 4, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  FastLED.setBrightness(255);
+
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+
+  FastLED.show();
+}
+
+void loop() {
+
+  for (int j=0; j<5; j++) {
+    color_beam();
+  }
+
+  flashes();
+
+  
+}
+
+void flashes() {
+  for (int i = 0; i < 5; i++) {
+    fill_solid(leds, NUM_LEDS, CRGB::Blue);
+    delay(100);
+
+    for (int j = 0; j < 40; j++) {
+      nscale8(leds, NUM_LEDS, 235);
+      FastLED.show();
+    }
+    if ((i == 1) || (i == 4)) {
+      delay(200);
+    }
+  }
+
+  for (int i = 0; i < 5; i++) {
+    fill_solid(leds, NUM_LEDS, CRGB::Red);
+    delay(100);
+
+    for (int j = 0; j < 40; j++) {
+      nscale8(leds, NUM_LEDS, 235);
+      FastLED.show();
+    }
+    if ((i == 1) || (i == 4)) {
+      delay(200);
+    }
+  }
+
+  for (int i = 0; i < 5; i++) {
+    fill_solid(leds, NUM_LEDS, CRGB::Yellow);
+    delay(100);
+
+    for (int j = 0; j < 40; j++) {
+      nscale8(leds, NUM_LEDS, 235);
+      FastLED.show();
+    }
+    if ((i == 1) || (i == 4)) {
+      delay(200);
+    }
+  }
+
+  for (int i = 0; i < 5; i++) {
+    fill_solid(leds, NUM_LEDS, CRGB::White);
+    delay(100);
+
+    for (int j = 0; j < 40; j++) {
+      nscale8(leds, NUM_LEDS, 235);
+      FastLED.show();
+    }
+    if ((i == 1) || (i == 4)) {
+      delay(200);
+    }
+  }
+}
+
+void color_beam() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+
+    leds[i] = CRGB::Blue;
+
+    FastLED.show();
+
+    nscale8(leds, NUM_LEDS, 245);
+  }
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+
+    leds[i] = CRGB::Red;
+
+    FastLED.show();
+
+    nscale8(leds, NUM_LEDS, 245);
+  }
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+
+    leds[i] = CRGB::Yellow;
+
+    FastLED.show();
+
+    nscale8(leds, NUM_LEDS, 245);
+  }
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+
+    leds[i] = CRGB::White;
+
+    FastLED.show();
+
+    nscale8(leds, NUM_LEDS, 245);
+  }
+}
